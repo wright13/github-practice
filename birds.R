fa451fed97ac841dc90c93b874aee8edcc7d9133
Sarah E. Wright <s.wright.618@gmail.com>
2022-02-10 09:43:48 -0800

    Bird checklist EDA

--- birds.R
+++ birds.R
@@ -7,3 +7,27 @@ birds <- read_csv("https://raw.githubusercontent.com/KateMMiller/IMD_R_Training_
 
 # Preview dataset
 birds
+
+# Verify that all species names are unique
+paste("There are", nrow(birds), "rows in this dataset.")
+paste("There are", nrow(distinct(birds)), "unique rows in this dataset.")
+
+# How many species groups are there?
+paste("There are", n_distinct(birds$species_group), "species groups.")
+
+# Any missing data?
+any(is.na(birds$species_group))
+any(is.na(birds$primary_com_name))
+
+# Count species in each species group
+group_size <- birds %>%
+  group_by(species_group) %>%
+  summarize(species_count = n()) %>%
+  arrange(-species_count) %>%
+  ungroup()
+
+# What are the largest species groups?
+group_size
+
+# What are the smallest species groups?
+tail(group_size)
