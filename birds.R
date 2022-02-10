69db269ed12c8f476087fc0baa2582ac63e4334d
Sarah E. Wright <s.wright.618@gmail.com>
2022-02-10 09:42:36 -0800

    Update data import to use Tidyverse

--- birds.R
+++ birds.R
@@ -1,5 +1,9 @@
+# Load packages
+library(dplyr)
+library(readr)
+
 # Read in bird checklist
-birds <- read.csv("https://raw.githubusercontent.com/KateMMiller/IMD_R_Training_Advanced/main/data/birds.csv")
+birds <- read_csv("https://raw.githubusercontent.com/KateMMiller/IMD_R_Training_Advanced/main/data/birds.csv")
 
 # Preview dataset
-head(birds)
+birds
